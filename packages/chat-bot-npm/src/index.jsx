89e67117b67480f20388be9660a0f448a72ee7df
Prashanta Pandit <prashanta927pandit@gmail.com>
2025-11-18 05:47:54 +0000

    remove inbuild react css

--- packages/chat-bot-npm/src/index.jsx
+++ packages/chat-bot-npm/src/index.jsx
@@ -5,8 +5,7 @@ import "./index.css";
 export default function ChatBotWidget() {
   return (
     <div
-      className="fixed bottom-4 right-4 w-16 h-16 bg-blue-600 text-white rounded-full shadow-lg
-                 flex items-center justify-center cursor-pointer hover:bg-blue-700 transition"
+        className="fixed bottom-7 right-7 w-16 h-16 bg-blue-600 text-white rounded-full shadow-lg flex items-center justify-center cursor-pointer hover:bg-blue-700 transition"
     >
       <MessageCircle size={28} />
     </div>
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ChatBotWidget from "../packages/chat-bot-npm/dist";
+import "./index.css";
+
 export default function App() {
   return (
-    <ChatBotWidget />
+    <>
+      <div className="bg-neutral-100 w-full min-h-screen flex items-center justify-center">
+        <h1 className="text-md font-semibold text-neutral-800">Test area for chat widgets</h1>
+      </div>
+      <ChatBotWidget />
+    </>
   );
 }
