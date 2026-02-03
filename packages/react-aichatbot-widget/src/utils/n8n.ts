b34548e718d8819b5c11c5457a14359c7e5b7a97
Prashanta Pandit <prashanta927pandit@gmail.com>
2026-02-03 01:42:11 +0000

    update the url for save message to the worker proxy

--- packages/react-aichatbot-widget/src/utils/n8n.ts
+++ packages/react-aichatbot-widget/src/utils/n8n.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const handleSaveMessages = async ( sessionId: string, chatbotNamespace: string ) => {
     try {
-        await axios.get(`https://n8n.clone67.com/webhook/end-chat-session?chatbotNamespace=${chatbotNamespace}&sessionId=${sessionId}`)
+        await axios.post(`https://request-n8n-end-chat-session.clone67.com/?chatbotNamespace=${chatbotNamespace}&sessionId=${sessionId}`)
     }
     catch(error){
         throw new Error;
