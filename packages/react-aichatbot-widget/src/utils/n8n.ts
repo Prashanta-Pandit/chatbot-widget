768b72dc0bca4ac62fd6f56036ed1f1bddca3287
Prashanta Pandit <prashanta927pandit@gmail.com>
2026-02-16 00:07:16 +0000

    add handleSaveMessages

---
+++ packages/react-aichatbot-widget/src/utils/n8n.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const handleSaveMessages = async ( sessionId: string, chatbotNamespace: string ) => {
+    try {
+        await axios.post(`https://request-n8n-end-chat-session.clone67.com/?chatbotNamespace=${chatbotNamespace}&sessionId=${sessionId}`)
+    }
+    catch(error){
+        throw new Error;
+    }
+}
